cb6ce7d32bd706fd6696452b2a724f89911cc6a5
davibelini <dev.dado.belini@gmail.com>
2020-12-20 23:59:08 -0300

    num data types

---
+++ numdatatypes/file1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // int
+    int dogs = 6;
+    printf("dogs: %i\n", dogs);
+
+    // float or double
+
+    double value = 1.1111111111111111111F;
+    printf("precise value: %f\n", value);
+
+    return 0;
+}
